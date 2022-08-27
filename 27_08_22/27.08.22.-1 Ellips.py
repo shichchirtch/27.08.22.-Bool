c81ce81c432d9bdb3eade1fd8713055b43cbbc89
shichchirtch <ivanvol9l@gmail.com>
2022-08-27 13:07:13 +0400

    init

---
+++ 27_08_22/27.08.22.-1 Ellips.py
@@ -0,0 +1,16 @@
+class Ellips:
+
+    def __init__(self, x1 = 0, y1 = 0, x2 = 0, y2 = 0):
+        if x1 !=0 and y1 != 0 and x2 !=0 and y2 != 0:
+            self.x1, self.y1, self.x2, self.y2 = x1, y1, x2, y2
+
+
+
+
+
+
+
+
+
+
+lst_geom = [Ellipse(), Ellipse(), Ellipse(1, 2, 3, 4), Ellipse(5, 6, 7, 8)]
