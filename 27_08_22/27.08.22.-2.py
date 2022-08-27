a629d3c7b7be501bd3e5b6a80b44b583baa12e40
shichchirtch <ivanvol9l@gmail.com>
2022-08-28 01:11:02 +0400

    Задача на +-*.

--- 27_08_22/27.08.22.-2.py
+++ 27_08_22/27.08.22.-2.py
@@ -5,14 +5,17 @@ class Vector:
             if isinstance(x, (int, float)):
                 self.arr.append(x)
     def __len__(self):
+        print("__len__")
         return len(self.arr)
+
     def compare_len(self, other):
         if len(self) == len(other):
             return True
-        raise ArithmeticError('размерности векторов не совпадают')
+        else:
+            raise ArithmeticError('размерности векторов не совпадают')
 
     def __add__(self, other):
-        if other.compare_len(other):
+        if self.compare_len(other):
             new_arr = []
             for x, y in zip(self.arr, other.arr):
                 new_arr.append(x+y)
@@ -20,7 +23,7 @@ class Vector:
             return new_vec
     def __iadd__(self, other):
         if type(other) == Vector:
-            if other.compare_len(other):
+            if self.compare_len(other):
                 for x in range (len(self.arr)):
                     self.arr[x] = self.arr[x]+other.arr[x]
                 return self
@@ -30,7 +33,7 @@ class Vector:
             return self
 
     def __sub__(self, other):
-        if other.compare_len(other):
+        if self.compare_len(other):
             new_arr = []
             for x, y in zip(self.arr, other.arr):
                 new_arr.append(x-y)
@@ -39,7 +42,7 @@ class Vector:
 
     def __isub__(self, other):
         if type(other) == Vector:
-            if other.compare_len(other):
+            if self.compare_len(other):
                 for x in range (len(self.arr)):
                     self.arr[x] = self.arr[x]-other.arr[x]
                 return self
@@ -47,19 +50,46 @@ class Vector:
             for x in range(len(self.arr)):
                 self.arr[x] = self.arr[x]-other
             return self
+    def __mul__(self, other):
+        if self.compare_len(other):
+            new_arr = []
+            for x, y in zip(self.arr, other.arr):
+                new_arr.append(x*y)
+            new_vec = Vector(*new_arr)
+            return new_vec
     def __eq__(self, other):
         return self.arr==other.arr
 
 
 
-p1 = [1,2,3]
-p2 = [0,2,3]
+
+p1 = [5,2,9]
+p2 = [1,2,3,]
 v1 = Vector(*p1)
 v2 = Vector(*p2)
-print(v1!=v2)
+print(v2.arr)
+print(len(v1), len(v2))
+# print(v1!=v2)
 v_res = v1-v2
-print(v_res.arr)
+print("v_res= ", v_res.arr)
 # v1+=10
-# print(v1.arr)
-v_res+=v1
-print(v_res.arr)
+# # print(v1.arr)
+# v_res+=v1
+# print(v_res.arr)
+v1 = Vector(1, 2, 3)
+v2 = Vector(4, 5, 6)
+print("****************************************************",(v1 + v2).arr)  # [5, 7, 9]
+print((v1 - v2).arr)  # [-3, -3, -3]
+print((v1 * v2).arr)  # [4, 10, 18]
+
+v1 += 10
+print(v1.arr)  # [11, 12, 13]
+v1 -= 10
+print(v1.arr)  # [1, 2, 3]
+v1 += v2
+print(v1.arr)  # [5, 7, 9]
+v2 -= v1
+print(v2.arr)  # [-1, -2, -3]
+
+print(v1 == v2)  # False
+print(v1 != v2)  # True
